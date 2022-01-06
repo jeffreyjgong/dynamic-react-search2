1ee704b809711510a6af7fa686c7ce4213d390fa
jeffreyjgong <jeffrey.gong7@gmail.com>
2022-01-05 20:25:43 -0500

    Initial commit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# dynamic-react-search2
+Following a tutorial on [Medium](https://medium.com/geekculture/create-a-simple-search-component-in-react-js-using-react-hooks-710c1dfe8b58).
---
+++ src/data/initialDetails.js
@@ -0,0 +1,39 @@
+const initialDetails = [
+   {
+     id: 1,
+     imgPath: "https://cdn.iconscout.com/icon/free/png-256/1-one-digital-number-numerical-numbers-36018.png",
+     name: "Jane Doe",
+     email: "janedoe@gmail.com",
+     address: "New Delhi, India",
+   },
+   {
+     id: 2,
+     imgPath: "https://icon-library.com/images/number-2-icon/number-2-icon-24.jpg",
+     name: "Mary Rosamund",
+     email: "agra@rosie.com",
+     address: "Tbilisi, India",
+   },
+   {
+     id: 3,
+     imgPath: "https://cdn.iconscout.com/icon/free/png-256/3-three-digital-number-numerical-numbers-36021.png",
+     name: "Sherlock Watson",
+     email: "irene@johnlock.com",
+     address: "Baker Street, India",
+   },
+   {
+     id: 4,
+     imgPath: "https://cdn.iconscout.com/icon/free/png-256/4-four-digital-number-numerical-numbers-36017.png",
+     name: "John Holmes",
+     email: "mary@johnlock.com",
+     address: "Baker Street, India",
+   },
+   {
+     id: 5,
+     imgPath: "https://icons.iconarchive.com/icons/iconarchive/red-orb-alphabet/256/Number-5-icon.png",
+     name: "Mycroft Lestrade",
+     email: "britishgovt@gmail.com",
+     address: "London, India",
+   },
+ ];
+ 
+ export default initialDetails;
